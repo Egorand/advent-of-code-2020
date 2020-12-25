1f59ca7259587ea2910e3e073240de54afcfeceb
Egor Andreevici <egor@squareup.com>
2020-12-25 14:45:34 -0500

    Day 24 - Part 1

---
+++ src/test/kotlin/dev/egorand/adventofcode/Day24Test.kt
@@ -0,0 +1,73 @@
+package dev.egorand.adventofcode
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Day24Test {
+  @Test fun `Part 1 - Examples`() {
+    assertEquals(Tile(6, 0), findTile("esenee"))
+    assertEquals(Tile(1, 1), findTile("esew"))
+    assertEquals(Tile(0, 0), findTile("nwwswee"))
+
+    assertEquals(
+      10,
+      flipTiles(
+        """
+        sesenwnenenewseeswwswswwnenewsewsw
+        neeenesenwnwwswnenewnwwsewnenwseswesw
+        seswneswswsenwwnwse
+        nwnwneseeswswnenewneswwnewseswneseene
+        swweswneswnenwsewnwneneseenw
+        eesenwseswswnenwswnwnwsewwnwsene
+        sewnenenenesenwsewnenwwwse
+        wenwwweseeeweswwwnwwe
+        wsweesenenewnwwnwsenewsenwwsesesenwne
+        neeswseenwwswnwswswnw
+        nenwswwsewswnenenewsenwsenwnesesenew
+        enewnwewneswsewnwswenweswnenwsenwsw
+        sweneswneswneneenwnewenewwneswswnese
+        swwesenesewenwneswnwwneseswwne
+        enesenwswwswneneswsenwnewswseenwsese
+        wnwnesenesenenwwnenwsewesewsesesew
+        nenewswnwewswnenesenwnesewesw
+        eneswnwswnwsenenwnwnwwseeswneewsenese
+        neswnwewnwnwseenwseesewsenwsweewe
+        wseweeenwnesenwwwswnew  
+        """.trimIndent()
+      ).countBlackTiles()
+    )
+  }
+
+  @Test fun `Part 1 - Input`() {
+    assertEquals(312, flipTiles(readText("inputs/day24")).countBlackTiles())
+  }
+
+  @Test fun `Part 2 - Examples`() {
+    val tiles = flipTiles(
+      """
+        sesenwnenenewseeswwswswwnenewsewsw
+        neeenesenwnwwswnenewnwwsewnenwseswesw
+        seswneswswsenwwnwse
+        nwnwneseeswswnenewneswwnewseswneseene
+        swweswneswnenwsewnwneneseenw
+        eesenwseswswnenwswnwnwsewwnwsene
+        sewnenenenesenwsewnenwwwse
+        wenwwweseeeweswwwnwwe
+        wsweesenenewnwwnwsenewsenwwsesesenwne
+        neeswseenwwswnwswswnw
+        nenwswwsewswnenenewsenwsenwnesesenew
+        enewnwewneswsewnwswenweswnenwsenwsw
+        sweneswneswneneenwnewenewwneswswnese
+        swwesenesewenwneswnwwneseswwne
+        enesenwswwswneneswsenwnewswseenwsese
+        wnwnesenesenenwwnenwsewesewsesesew
+        nenewswnwewswnenesenwnesewesw
+        eneswnwswnwsenenwnwnwwseeswneewsenese
+        neswnwewnwnwseenwseesewsenwsweewe
+        wseweeenwnesenwwwswnew  
+        """.trimIndent()
+    )
+    assertEquals(10, simulateLivingArt(tiles, days = 0).countBlackTiles())
+    assertEquals(15, simulateLivingArt(tiles, days = 1).countBlackTiles())
+  }
+}
